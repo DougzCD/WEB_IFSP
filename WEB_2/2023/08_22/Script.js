99a47c1b76b4cedf8392ea6354afe238969529e1
DougzCD <magonegrodcd@gmail.com>
2023-08-22 09:27:58 -0300

    sei la

--- WEB_2/2023/08_22/Script.js
+++ WEB_2/2023/08_22/Script.js
@@ -3,14 +3,35 @@ const app = express()
 
 const porta = 80;
 
+app.get('/',(req, res)=>{
+    res.send("<h1>Home</h1>");
+})
+
+app.get('/:parametro',(req, res)=>{
+    const {parametro} = req.params;
+    res.send(`<h1>${parametro}+</h1>`);
+})
+
+app.get('/Gatos',(req, res)=>{
+    res.send("<h1>Gatos</h1>");
+})
+
+app.get('/Carros',(req, res)=>{
+    res.send("<h1>Carros</h1>");
+})
+
+app.get('/Soccer',(req, res)=>{
+    res.send("<h1>Soccer</h1>");
+})
+
+app.get('*',(req, res)=>{
+    res.send("<h1>Sei la nem existe</h1>");
+})
+
 app.use((req,res)=>{
     console.log('Recebi requisicao');
     res.send('Resposta servidor');
 });
-
-app.get('/',(req, res)=>{
-    res.send("<h1>Sei la</h1>");
-})
 
 /*
     REST
